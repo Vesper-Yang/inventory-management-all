372e7e9b3522f53bc6748d72518eb4e83497529a
Vesper Yang <43669090+Vesper-Yang@users.noreply.github.com>
2024-10-09 17:05:56 +0800

    菜单Dashboard的PurchaseSummary、SalesSummary板块，购买、销售的统计图表，使用recharts库

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 
 import CardPopularProducts from "./CardPopularProducts"
+import CardPurchaseSummary from "./CardPurchaseSummary"
+import CardSalesSummary from "./CardSalesSummary"
 
 const Dashboard = () => {
   return (
@@ -8,8 +10,8 @@ const Dashboard = () => {
       className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows "
     >
       <CardPopularProducts />
-      <div className="row-span-3 xl:row-span-6 bg-gray-500"/>
-      <div className="row-span-2 xl:row-span-3 bg-gray-500 col-span-1 md:col-span-2 xl:col-span-1"/>
+      <CardSalesSummary />
+      <CardPurchaseSummary />
       <div className="row-span-3 bg-gray-500"/>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
       <div className="md:row-span-1 xl:row-span-2 bg-gray-500"/>
